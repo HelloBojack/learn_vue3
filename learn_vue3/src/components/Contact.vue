e149a97890718846d4ccf1ac745dfbb243ed49d9
bojack <695668678@qq.com>
2021-12-30 21:03:48 +0800

    $ref

--- learn_vue3/src/components/Contact.vue
+++ learn_vue3/src/components/Contact.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+import {ref,onMounted} from 'vue'
+const contactDiv=ref()
+onMounted(()=>{
+  console.log('mounted')
+  console.log(contactDiv.value)
+})
+</script>
 <template>
   Contact
+  <div id='contact' ref='contactDiv'>contact</div>
 </template>
