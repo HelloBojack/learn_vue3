4b101e2bbf7112a5671226b656a5ee1ddc47fc7a
bojack <695668678@qq.com>
2021-12-30 20:01:28 +0800

    slot+component

--- learn_vue3/src/App.vue
+++ learn_vue3/src/App.vue
@@ -1,13 +1,48 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import { reactive,shallowRef,ref,markRaw } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
-let msg = 'Hello Vue!'
+import MyTabs from './components/MyTabs.vue'
+import Home from './components/Home.vue'
+import About from './components/About.vue'
+import Contact from './components/Contact.vue'
+let currentTab = ref(Home)
+let currentTabName = ref('Home')
+let tabs=[
+  {
+    name: 'Home',
+    path: '/',
+    component: Home
+  },
+  {
+    name: 'About',
+    path: '/about',
+    component: About
+  },
+  {
+    name: 'Contact',
+    path: '/contact',
+    component: Contact
+  }
+]
+const click = (e:{name:string,path:string,component:any})=>{
+  currentTab.value=markRaw(e.component)
+  currentTabName.value=e.name
+}
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld :msg=msg />
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+
+  <MyTabs :tabs="tabs" >
+  <template v-slot:tab="{item}">
+    <button @click="click(item)" :class="item.name===currentTabName?'active':''">{{item.name}}</button>
+  </template>
+  </MyTabs>
+  <br>
+  <component :is="currentTab"></component>
+  <!-- <HelloWorld :msg="msg" /> -->
 </template>
 
 <style>
@@ -18,5 +53,8 @@ let msg = 'Hello Vue!'
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.active{
+  color:red
 }
 </style>
---
+++ learn_vue3/src/components/MyTabs.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+defineProps<{ tabs: Array<{name:string,path:string,component:any}> }>()
+
+</script>
+
+<template>
+  <template v-for="item in tabs">
+    <slot name="tab" :item="item"></slot>
+  </template>
+</template>
