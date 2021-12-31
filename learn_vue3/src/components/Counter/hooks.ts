14d70eccc9f622a6758d3f46b23c5dcfeb4200a1
bojack <695668678@qq.com>
2021-12-31 14:06:05 +0800

    useTitle

--- learn_vue3/src/components/Counter/hooks.ts
+++ learn_vue3/src/components/Counter/hooks.ts
@@ -1,7 +1,16 @@
-import { ref } from "vue";
+import { ref, watch } from "vue";
 
-export default function () {
+export const useCounter = function () {
   let count = ref(0);
   const increment = () => count.value++;
   return { count, increment };
-}
+};
+
+export const useTitle = () => {
+  let titleRef = ref("");
+  const setTitle = (title: string) => (titleRef.value = title);
+  watch(titleRef, (newTitle) => {
+    document.title = newTitle;
+  });
+  return { titleRef, setTitle };
+};
--- learn_vue3/src/components/Counter/index.vue
+++ learn_vue3/src/components/Counter/index.vue
@@ -1,11 +1,12 @@
-<script setup lang='ts'>
-import useCounter from './hooks'
-let { count, increment } = useCounter()
+<script setup lang="ts">
+import { useCounter, useTitle } from "./hooks";
+let { count, increment } = useCounter();
+let { titleRef, setTitle } = useTitle();
 </script>
 <template>
   <div>
-   {{count}}
+    {{ count }}
   </div>
   <button @click="increment">increment</button>
-
+  <button @click="setTitle('title')">title</button>
 </template>
