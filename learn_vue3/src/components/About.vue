d1e9f1874446eb3acba79e9f5f4e55c59afba7ec
bojack <695668678@qq.com>
2021-12-31 17:58:37 +0800

    v-date format

--- learn_vue3/src/components/About.vue
+++ learn_vue3/src/components/About.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts"></script>
 <template>
   <div v-date="{ format: 'YYYY-MM-DD' }">{{ Date.now() }}</div>
+  <div v-date="{ format: 'YYYY-MM-DD hh:mm:ss' }">{{ Date.now() }}</div>
+  <div>{{ Date.now() }}</div>
 </template>
--- learn_vue3/src/main.ts
+++ learn_vue3/src/main.ts
@@ -7,10 +7,19 @@ app.directive("focus", {
     el.focus();
   },
 });
-function dateFormat(fmt, date) {
+function dateFormat(fmt = "YYYY-MM-DD", date: string | Date) {
   let ret;
-  date = new Date(Number(date));
-  const opt = {
+  date = new Date(
+    (date as string).length >= 13 ? Number(date) : Number(date) * 1000
+  );
+  const opt: {
+    Y: string;
+    M: string;
+    D: string;
+    h: string;
+    m: string;
+    s: string;
+  } = {
     Y: date.getFullYear().toString(), // 年
     M: (date.getMonth() + 1).toString(), // 月
     D: date.getDate().toString(), // 日
@@ -21,9 +30,12 @@ function dateFormat(fmt, date) {
   for (let k in opt) {
     ret = new RegExp("(" + k + "+)").exec(fmt);
     if (ret) {
+      console.log(ret[1], fmt);
       fmt = fmt.replace(
         ret[1],
-        ret[1].length == 1 ? opt[k] : opt[k].padStart(ret[1].length, "0")
+        ret[1].length == 1
+          ? opt[k as keyof typeof opt]
+          : opt[k as keyof typeof opt].padStart(ret[1].length, "0")
       );
     }
   }
