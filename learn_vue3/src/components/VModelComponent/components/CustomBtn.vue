f3954c3c759ff218910d11f287cfb6796a0c1a43
bojack <695668678@qq.com>
2021-12-31 16:17:51 +0800

    v-focus

--- learn_vue3/src/components/VModelComponent/components/CustomBtn.vue
+++ learn_vue3/src/components/VModelComponent/components/CustomBtn.vue
@@ -1,7 +1,12 @@
-<script setup lang='ts'>
-defineProps({modelValue:Number})
-defineEmits(["update:modelValue"])
+<script setup lang="ts">
+defineProps({ modelValue: Number });
+defineEmits(["update:modelValue"]);
 </script>
 <template>
-  <input type="text" :value="modelValue" @input="$emit('update:modelValue',$event.target.value)">
+  <input
+    v-focus
+    type="text"
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+  />
 </template>
--- learn_vue3/src/main.ts
+++ learn_vue3/src/main.ts
@@ -1,4 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.directive("focus", {
+  mounted(el) {
+    el.focus();
+  },
+});
+app.mount("#app");
