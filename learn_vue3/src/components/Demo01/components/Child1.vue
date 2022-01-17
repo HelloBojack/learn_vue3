8c481d9cf6ad24313bdafa65ba855c5bffea47c8
bojack <695668678@qq.com>
2022-01-17 15:11:30 +0800

    v-model

---
+++ learn_vue3/src/components/Demo01/components/Child1.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const props = defineProps({
+  text: {
+    type: String,
+  },
+});
+const emits = defineEmits(["update:text"]);
+</script>
+<template>
+  <input
+    type="text"
+    :value="props.text"
+    @input="(e) => emits('update:text', e.target.value)"
+  />
+</template>
--- learn_vue3/src/components/Demo01/index.vue
+++ learn_vue3/src/components/Demo01/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import Child from "./components/Child.vue";
+import Child1 from "./components/Child1.vue";
 let text = ref("Father Text");
 const changeText = (value: string) => {
   console.log("changeText", value);
@@ -10,4 +11,5 @@ const changeText = (value: string) => {
 <template>
   <div>Demo Father</div>
   <Child :text="text" @changeText="changeText"></Child>
+  <Child1 v-model:text="text"></Child1>
 </template>
