a0863f376247467215038327820e2eb99fbb8e7c
bojack <695668678@qq.com>
2022-01-17 15:25:32 +0800

    defineExpose

---
+++ learn_vue3/src/components/Demo01/components/Child2.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+let text = ref("Child2 Text");
+const changeText = () => {
+  text.value = text.value + "1";
+};
+defineExpose({
+  text,
+  changeText,
+});
+</script>
+<template>
+  <div>child2</div>
+  <div>{{ text }}</div>
+  <button @click="changeText">change</button>
+</template>
--- learn_vue3/src/components/Demo01/index.vue
+++ learn_vue3/src/components/Demo01/index.vue
@@ -1,15 +1,26 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
 import Child from "./components/Child.vue";
 import Child1 from "./components/Child1.vue";
+import Child2 from "./components/Child2.vue";
 let text = ref("Father Text");
 const changeText = (value: string) => {
   console.log("changeText", value);
   text.value = value;
 };
+const refChild = ref(null);
+onMounted(() => {
+  console.log(refChild.value.text);
+});
+const changeChild2Text = () => {
+  // refChild.value.text = refChild.value.text + "1";
+  refChild.value.changeText();
+};
 </script>
 <template>
   <div>Demo Father</div>
+  <button @click="changeChild2Text">changeChild2Text</button>
   <Child :text="text" @changeText="changeText"></Child>
   <Child1 v-model:text="text"></Child1>
+  <Child2 ref="refChild"></Child2>
 </template>
