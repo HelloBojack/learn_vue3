93356abc74e6e2d9603ec1e8a6c17623a59cb20f
bojack <695668678@qq.com>
2022-02-14 20:31:35 +0800

    vue2

--- mini_vue/vue.js
+++ mini_vue/vue.js
@@ -1,6 +1,8 @@
 class Vue {
   constructor(options) {
     let { el, data, beforeCreate, created, beforeMount, mounted } = options;
+    this.$options = options;
+    this.proxyData()
     beforeCreate && beforeCreate.call(this)
     this.$data = data
     created && created.call(this)
@@ -9,11 +11,32 @@ class Vue {
     mounted && mounted.call(this)
     this.complie(this.$el)
   }
-
+  proxyData() {
+    for (const key in this.$data) {
+      Object.defineProperty(this, key, {
+        get() {
+          return this.$data[key]
+        },
+        set(newValue) {
+          this.$data[key] = newValue
+        }
+      })
+    }
+  }
   complie(el) {
     el.childNodes.forEach(node => {
       // node
       if (node.nodeType === 1) {
+        console.dir(node);
+        if (node.attributes.length > 0) {
+          let attr = node.attributes[0];
+          if (attr.name === '@click') {
+            let name = attr.value;
+            node.addEventListener('click', e => {
+              this.$options.methods[name].call(this, e)
+            })
+          }
+        }
         this.complie(node)
       }
       // 文本
